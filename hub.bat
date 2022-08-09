2b08cf40f3b2a4dcc6242e9f5fd9ee8174ff377e
Rupesh510 <100418196+Rupesh510@users.noreply.github.com>
2022-08-09 11:57:30 +0530

    Add files via upload

---
+++ hub.bat
@@ -0,0 +1,1 @@
+java -jar c:\seleniumgrid\selenium-server-standalone-3.141.59.jar -role hub
---
+++ node.bat
@@ -0,0 +1,1 @@
+java -Dwebdriver.chrome.driver=c:\seleniumgrid\chromedriver.exe -Dwebdriver.gecko.driver=c:\seleniumgrid\geckodriver.exe -jar c:\seleniumgrid\selenium-server-standalone-3.141.59.jar -role webdriver -hub http://192.168.1.160:4444/grid/register
